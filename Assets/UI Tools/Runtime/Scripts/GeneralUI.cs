561f5426e31b477c254281d0fe3ec99e15db4f4f
IOmeow <iomeow11325@gmail.com>
2025-09-01 16:52:35 +0800

    update ui tools

---
+++ Assets/UI Tools/Runtime/Scripts/GeneralUI.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace IOmeow.UItools
+{
+    public static class GeneralUI
+    {
+        public static void FadeGraphic(Graphic graphic, float dstA, float rate)
+        {
+            Color c = graphic.color;
+            float a = Mathf.SmoothStep(c.a, dstA, rate);
+
+            if (a > 0.99f) a = 1;
+            else if (a < 0.01f) a = 0;
+
+            c.a = a;
+            graphic.color = c;
+        }
+    }
+}
